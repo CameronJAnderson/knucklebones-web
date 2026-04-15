2f38f55f8cde6bac88fccdda77d4ed069d437b73
CameronJAnderson <canderson49@fingerlakes.edu>
2026-04-15 12:51:14 -0400

    Server Fix?

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const WebSocket = require("ws");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../client")));
 
